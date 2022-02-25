e2e1f62e84f252d73ea173470c0ef130dcadda54
topguru91-git <aleh.baikou91@gmail.com>
2022-02-24 19:45:12 -0800

    update runpipeline

--- billtitleindex/scrapeindex/management/commands/runpipeline.py
+++ billtitleindex/scrapeindex/management/commands/runpipeline.py
@@ -1,12 +1,29 @@
-from lib2to3.pytree import Base
 from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
+import logging
+
+from scrapeindex import utils
+
 
 class Command(BaseCommand):
-    help = 'Run pipeline to load bill title data into PostgreSQL DB and index them.'
+    help = 'Run pipeline to load billstatus title data into PostgreSQL DB and index them.'
     
     def add_arguments(self, parser):
-        return super().add_arguments(parser)
+        parser.add_argument('bill_id', nargs='+', type=str)
+        
     
     def handle(self, *args, **options):
-        return super().handle(*args, **options)
+        bill_id = options.get('bill_id', None)
+        if bill_id:
+            to_fetch = bill_id.split(",")
+        else:
+            to_fetch = utils.get_json_bills_to_process(options)
+            
+            if not to_fetch:
+                logging.warn("All bills indexed.")
+                return None
+            
+            limit = options.get('limit', None)
+            if limit:
+                to_fetch = to_fetch[:int(limit)]
+        
+        utils.process_set(to_fetch, options)
