a9ba2df561879e0c4177a1bf66007cf23af27304
topguru91-git <aleh.baikou91@gmail.com>
2022-02-27 07:27:15 -0800

    update models

--- billtitleindex/billtitleindex/settings/dev.py
+++ billtitleindex/billtitleindex/settings/dev.py
@@ -1,11 +1,12 @@
 from decouple import config
+from .base import *
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("DEV_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ["localhost"]
--- billtitleindex/billtitleindex/settings/prod.py
+++ billtitleindex/billtitleindex/settings/prod.py
@@ -1,7 +1,7 @@
 from decouple import config
-
+from .base import *
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('PROD_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
